8fa76e9b56417fc806539e132351a5b12f2c3e42
DB <131527960+Siucra@users.noreply.github.com>
2026-03-10 11:33:48 +0000

    Node.java & Bike.java
    Added the skeleton code for both Bike.java and Node.java

--- BikeParkingManager/src/Bike.java
+++ BikeParkingManager/src/Bike.java
@@ -6,5 +6,36 @@
  */
 
 public class Bike {
+    private int bikeId;
+    private String ownerName;
+
+   // public Bike() {//NULL CONSTRUCTOR
+    //}
+
+    //public Bike(int bikeId, String ownerName) {
+      //  this.bikeId = bikeId;
+        //this.ownerName = ownerName;
+    //}
+
+    public int getBikeId() {
+        return bikeId;
+    }
+
+    public void setBikeId(int bikeId) {
+        this.bikeId = bikeId;
+    }
+
+    public String getOwnerName() {
+        return ownerName;
+    }
+
+    public void setOwnerName(String ownerName) {
+        this.ownerName = ownerName;
+    }
+    
+    public String toString(){
+    return "Bike id: "+bikeId+", Owner name: "+ownerName;
+    }
+    
     
 }
--- BikeParkingManager/src/Node.java
+++ BikeParkingManager/src/Node.java
@@ -6,5 +6,33 @@
  */
 
 public class Node {
+    private Object element;
+    private Node next;
+
+    public Node() {
+        next = null;
+        element = null;
+    }
+
+    public Node(Object element) {
+        this.element = element;
+        this.next = null;
+    }
+
+    public Object getElement() {
+        return element;
+    }
+
+    public void setElement(Object element) {
+        this.element = element;
+    }
+
+    public Node getNext() {
+        return next;
+    }
+
+    public void setNext(Node next) {
+        this.next = next;
+    }
     
 }
